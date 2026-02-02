97f8a1dbaaf7c1e0997174d962aec8f1915b7027
Farhan Khalawi <farhankhalawi20@gmail.com>
2026-02-02 14:25:52 +0100

    Add dataset reading script

---
+++ data/read_dataset.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df_train = pd.read_csv("data/raw/pubmed_train.csv")
+#df_valid = pd.read_csv("data/raw/pubmed_validation.csv")
+#df_test  = pd.read_csv("data/raw/pubmed_test.csv")
+
+print(df_train.head())
