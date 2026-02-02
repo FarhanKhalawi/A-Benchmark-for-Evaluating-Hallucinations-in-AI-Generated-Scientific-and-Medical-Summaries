de73fe99ed8b4e98bdbca1d2c2664e5f35805fdc
Farhan Khalawi <farhankhalawi20@gmail.com>
2026-02-02 14:22:53 +0100

    Add main entry point

---
+++ src/main.py
@@ -0,0 +1,7 @@
+PROJECT_NAME = "A Benchmark for Evaluating Hallucinations in AI-Generated Scientific and Medical Summaries"
+
+def main():
+    print(PROJECT_NAME)
+
+if __name__ == "__main__":
+    main()
