6cf7fe9b8b4070a326f2bc4fef923ebdf7175342
Farhan Khalawi <farhankhalawi20@gmail.com>
2026-03-03 16:34:08 +0100

    Update read_dataset.py

--- data/read_dataset.py
+++ data/read_dataset.py
@@ -4,4 +4,5 @@ df_train = pd.read_csv("data/raw/pubmed_train.csv")
 #df_valid = pd.read_csv("data/raw/pubmed_validation.csv")
 #df_test  = pd.read_csv("data/raw/pubmed_test.csv")
 
+#df_train = pd.read_csv("data/processed/pubmed_train_clean.csv")
 print(df_train.head())
