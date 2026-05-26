8e3bae04ed0be98f4dd627370339c54b246538ea
Farhan Khalawi <farhankhalawi20@gmail.com>
2026-05-26 15:29:30 +0200

    Update dataset reading utility

--- data/read_dataset.py
+++ data/read_dataset.py
@@ -6,3 +6,10 @@ df_train = pd.read_csv("data/raw/pubmed_train.csv")
 
 #df_train = pd.read_csv("data/processed/pubmed_train_clean.csv")
 print(df_train.head())
+
+###############################################################
+import pandas as pd
+
+df = pd.read_csv("data/processed/pubmed_train_clean_tokens(1000).csv")
+row = df.iloc[551]
+print(row["article"][:1000])
